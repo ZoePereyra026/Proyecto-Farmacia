8fca4c8dbde227f08e54d15b88352001c635d298
ZoePereyra026 <zoepereyra026@gmail.com>
2025-07-30 11:16:01 -0300

    Agregar los endpoints para registrar e iniciar sesion

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 require("dotenv").config();
 
+
 const connectDB = require("./config/database");
-const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,13 +16,13 @@ app.use(express.json());
 
 // Ruta principal
 app.get("/", (req, res) =>
-  res.json({ message: "Bienvenidos a la API REST de eCommerc - Farmacia San Martín!" })
+  res.json({ message: "Bienvenidos a la API REST de eCommerce - Farmacia San Martín!" })
 );
 
-// Rutas de autenticación
-app.use("/api/auth", authRoutes);
+// Rutas de los usuarios
+app.use("/api/usuarios", userRoutes);
 
 // Iniciar servidor
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en: http://localhost:${PORT}`);
+  console.log(` Servidor inicializado en: http://localhost:${PORT}`);
 });
