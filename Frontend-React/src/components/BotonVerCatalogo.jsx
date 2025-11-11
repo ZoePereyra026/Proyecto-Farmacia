d9be18dd2ffe7907c5382ad96574e0f935f692ba
Zoe <zoepereyra026@gmail.com>
2025-11-11 12:53:10 -0300

    Agregar diseño responsive al boton ver catalogo

--- Frontend-React/src/components/BotonVerCatalogo.jsx
+++ Frontend-React/src/components/BotonVerCatalogo.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../../css/style_navbar.css';
 
 export default function BotonVerCatalogo() {
   return (
     <div className="boton-volver text-center mt-4 mb-5">
-      <Link to="/productos" className="btn-volver" style={{margin:'0px'}}>
+      <Link to="/productos" className="btn-volver" style={{marginTop:'20px'}}>
         Ver el catálogo
       </Link>
     </div>
